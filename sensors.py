23a4dd585ebf9ffc4f49b6a8730b241c81ed23d5
robz25 <weiserfi@outlook.com>
2017-03-12 22:27:18 -0600

    new file with data

---
+++ sensors.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import time, mraa
+from ubidots import ApiClient
+
+#Connect to Ubidots
+
+for i in range(0,5):
+    try:
+        print "Connecting to Ubidots"
+        api = ApiClient(token='9a50f3a5664b25e9d84dd05a2b20ed95e2e5dfff') # Replace with your Ubidots Token here
+        break
+
+    except:
+        print "No internet connection, retrying..."
+        time.sleep(5)
+
+# Assign analog pins
+
+a0 = mraa.Aio(0)
+a1 = mraa.Aio(1)
+
+# Read analog pins and send values to Ubidots
+while(1):
+    try:
+        api.save_collection([{'variable': '58c601877625420177532bae','value':a0.read()}, {'variable': '58c6018f7625420177532c05','value':a1.read()}])
+    except:
+        print("Couldn't send data.")
+        continue

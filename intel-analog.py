f25d10a3ab062f6b03461d85e4ec31027d101d2a
robz25 <weiserfi@outlook.com>
2017-03-12 19:40:39 -0600

    envio de datos de sensores a db

---
+++ intel-analog.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import time, mraa
+from ubidots import ApiClient
+
+#Connect to Ubidots
+
+for i in range(0,5):
+    try:
+        print "Requesting Ubidots token"
+        api = ApiClient('abebasfaf4e14d195c0044fcasdfdf9dsfab9d653af3')
+        break
+        # Replace with your Ubidots API Key here
+    except:
+        print "No internet connection, retrying..."
+        time.sleep(5)
+
+a0 = mraa.Aio(0)
+a1 = mraa.Aio(1)
+
+while(1):
+  api.save_collection([{'variable': '558073727625425555af27e4','value':a0.read()}, {'variable': '5580737876254257514be1e6','value':a1.read()}])
+

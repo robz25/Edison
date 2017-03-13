ffc05cbe32f64be3288b4f4e10a7c1f249e47964
robz25 <weiserfi@outlook.com>
2017-03-12 21:15:51 -0600

    actualizacion de codigos

--- intel-analog.py
+++ intel-analog.py
@@ -8,7 +8,7 @@ from ubidots import ApiClient
 for i in range(0,5):
     try:
         print "Requesting Ubidots token"
-        api = ApiClient('abebasfaf4e14d195c0044fcasdfdf9dsfab9d653af3')# Replace with your Ubidots API Key here
+        api = ApiClient('9a50f3a5664b25e9d84dd05a2b20ed95e2e5dfff')# Replace with your Ubidots API Key here
         break
         
     except:
@@ -19,5 +19,5 @@ a0 = mraa.Aio(0)
 a1 = mraa.Aio(1)
 #reemplazar numeros por el ID de mis variables
 while(1):
-  api.save_collection([{'variable': '558073727625425555af27e4','value':a0.read()}, {'variable': '5580737876254257514be1e6','value':a1.read()}])
+  api.save_collection([{'variable': 'a0','value':a0.read()}, {'variable': 'a1','value':a1.read()}])
 

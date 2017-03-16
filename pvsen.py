6db98404929970f006af43cff55c91dc9d6028df
robz25 <weiserfi@outlook.com>
2017-03-16 17:20:58 -0600

    .read() para acceder al valor crudo del objeto Aio

--- pvsen.py
+++ pvsen.py
@@ -11,8 +11,8 @@ try:
   while(1):
     a0 = mraa.Aio(0)
     a1 = mraa.Aio(1)
-    print "Sen 1: ", a0
-    print "Sen 2: ", a1
+    print "Sen 1: ", a0.read()
+    print "Sen 2: ", a1.read()
     #print1 = pvc+" "+sen1+" "+'"'+a0+'"'
     #print2 = pvc+" "+sen2+" "+'"'+a1+'"'
 #    print print1

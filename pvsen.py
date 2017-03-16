9c9157a025897575aaebe077e42ce249f57122e2
robz25 <weiserfi@outlook.com>
2017-03-16 17:11:21 -0600

    quitar string

--- pvsen.py
+++ pvsen.py
@@ -1,6 +1,6 @@
 import os, time, mraa
 
-string init = 'pvcloud init "https://costaricamakers.com/pvcloud/backend/vse.php" 104 d063c414e35456bb54db9c4589699af6869bd272 "Robinson"'
+init = 'pvcloud init "https://costaricamakers.com/pvcloud/backend/vse.php" 104 d063c414e35456bb54db9c4589699af6869bd272 "Robinson"'
 sen1 = '"Sensor de luz"'
 sen2 = '"Sensor de temperatura"'
 pvc = 'pvcloud write '

ad4127284fbf3be566380bee4ada3ff3ed728cfd
robz25 <weiserfi@outlook.com>
2017-03-16 17:24:26 -0600

    correcciones con .read()

--- pvsen.py
+++ pvsen.py
@@ -11,14 +11,12 @@ try:
   while(1):
     a0 = mraa.Aio(0)
     a1 = mraa.Aio(1)
-    print "Sen 1: ", a0.read()
-    print "Sen 2: ", a1.read()
-    #print1 = pvc+" "+sen1+" "+'"'+a0+'"'
-    #print2 = pvc+" "+sen2+" "+'"'+a1+'"'
-#    print print1
- #   print print2
-  #  os.system(print1)
-   # os.system(print2)
+    print1 = pvc+" "+sen1+" "+'"'+a0.read()+'"'
+    print2 = pvc+" "+sen2+" "+'"'+a1.read()+'"'
+    print print1
+    print print2
+    os.system(print1)
+    os.system(print2)
     
 except KeyboardInterrupt:
   #GPIO.cleanup()

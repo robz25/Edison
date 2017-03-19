3cfb38a0c6602f20a32293f8ef409f3e7744507c
robinson-root <robin.maker@outlook.com>
2017-03-19 19:25:30 +0000

    proyecto con blynk

---
+++ blynk.js
@@ -0,0 +1,18 @@
+var Blynk = require('blynk-library');
+var AUTH = '214447131de847268b093d81cf1da002';
+
+var blynk = new Blynk.Blynk(AUTH, options = {
+	connector : new Blynk.TcpClient()
+});
+
+var v1 = new blynk.VirtualPin(1);
+var v9 = new blynk.VirtualPin(9);
+
+v1.on('write', function(param){
+	console.log('V1:', param[0]);
+});
+
+v9.on('read', function(){
+	v9.write(new Date().getSeconds());
+});
+

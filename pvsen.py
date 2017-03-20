5cd124b0dbcaf18e2c25cc72305f48a6fb237dc7
robz25 <weiserfi@outlook.com>
2017-03-20 13:01:06 -0600

    conversion a temperatura en C

--- pvsen.py
+++ pvsen.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
+#Script de Python para enviar datos de sensores a PVcloud usando Edison
+#Autor Robin Gonzalez
 
-import os, time, mraa
+import os, time, mraa, math
 from ubidots import ApiClient#solo necesario para el call GPIO.cleanup()
+
+#Variables de conversion
+#ref: http://wiki.seeed.cc/Grove-Temperature_Sensor_V1.2/
+B = 4275
+r0 = 100000#resisterncia desconectado
 
 init = 'pvcloud init "https://costaricamakers.com/pvcloud/backend/vse.php" 104 d063c414e35456bb54db9c4589699af6869bd272 "Robinson"'
 sen1 = '"Sensor de luz"'
-sen2 = '"Sensor de temperatura"'
+sen2 = '"Temperatura °C:"'
 sen3 = '"Potenciometro"'
 pvc = 'pvcloud write '
 
@@ -16,8 +23,13 @@ try:
     a0 = mraa.Aio(0)
     a1 = mraa.Aio(1)
     a2 = mraa.Aio(2)
+    
+    r = 1023/a1.read()-1
+    r = r*r0
+    temperatura = 1/(math.log(r/r0)/B+1/298.15)-273.15
+    
     print1 = pvc+" "+sen1+" "+'"'+str(a0.read())+'"'
-    print2 = pvc+" "+sen2+" "+'"'+str(a1.read())+'"'
+    print2 = pvc+" "+sen2+" "+'"'+str(temperatura)+'"'
     print3 = pvc+" "+sen3+" "+'"'+str(a2.read())+'"'
     print print1
     print print2

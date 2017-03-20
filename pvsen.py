ca615bd74963b50596d61683569b69c42c403e26
robz25 <weiserfi@outlook.com>
2017-03-20 11:58:27 -0600

    actualizado para tres sensores

--- pvsen.py
+++ pvsen.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 import os, time, mraa
-from ubidots import ApiClient
+from ubidots import ApiClient#solo necesario para el call GPIO.cleanup()
 
 init = 'pvcloud init "https://costaricamakers.com/pvcloud/backend/vse.php" 104 d063c414e35456bb54db9c4589699af6869bd272 "Robinson"'
 sen1 = '"Sensor de luz"'
 sen2 = '"Sensor de temperatura"'
+sen3 = '"Potenciometro"'
 pvc = 'pvcloud write '
 
 os.system(init)
@@ -14,12 +15,16 @@ try:
   while(1):
     a0 = mraa.Aio(0)
     a1 = mraa.Aio(1)
+    a2 = mraa.Aio(2)
     print1 = pvc+" "+sen1+" "+'"'+str(a0.read())+'"'
     print2 = pvc+" "+sen2+" "+'"'+str(a1.read())+'"'
+    print3 = pvc+" "+sen3+" "+'"'+str(a2.read())+'"'
     print print1
     print print2
+    print print3
     os.system(print1)
     os.system(print2)
+    os.system(print3)
     
 except KeyboardInterrupt:
   GPIO.cleanup()
